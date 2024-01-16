57b1b02e27656fe777655f5f039a586248772938
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-01-16 13:52:21 -0300

    requisito not found

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('NotFound', () => {
+  it("deve conter um heading h2 com o texto 'Page requested not found'", () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading.textContent).toBe('Page requested not found');
+  });
+  it("deve mostrar a imagem com o texto alternativo 'Clefairy pushing buttons randomly with text I have no idea what i'm doing'", () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText("Clefairy pushing buttons randomly with text I have no idea what i'm doing");
+    expect(image).toBeInTheDocument();
+  });
+});
